adf5e8a858be04c6b0ba0447ba4d545b6e16bd2e
Esteban Flores <estflores.gt@gmail.com>
2024-06-15 01:07:01 -0600

    Styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,14 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Newsletter Subscription</title>
-        <meta name="description" content="Subscribe to our newsletter" />
+        <title>Frontend Technical Test</title>
+        <meta name="description" content="landscapes" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center min-h-screen py-2">
-        <h1 className="text-4xl font-bold mb-4">Subscribe to our Newsletter</h1>
-        <SubscribeForm />
+      <main className='flex flex-col'>
         <Carousel autoSlide={true} autoSlideInterval={3000} />
+        <SubscribeForm />
       </main>
     </div>
   )
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -6,19 +6,23 @@ import Image from 'next/image'
 const slides = [
   {
     image: '/images/slide1.jpg',
-    text: 'Slide 1 Text',
+    title: 'Experience the Beauty of Nature',
+    subtitle: 'Discover scenic trails, breathtaking views, and hidden gems in the wilderness',
   },
   {
     image: '/images/slide2.jpg',
-    text: 'Slide 2 Text',
+    title: 'Discover Amazing Places',
+    subtitle: 'Enjoy thrilling adventures like hiking, camping, kayaking, and wildlife spotting',
   },
   {
     image: '/images/slide3.jpg',
-    text: 'Slide 3 Text',
+    title: 'Adventure Awaits',
+    subtitle: 'Unplug and unwind in forests, mountains, and rivers while reconnecting with nature',
   },
   {
     image: '/images/slide4.jpg',
-    text: 'Slide 4 Text',
+    title: 'Embrace the Unkown',
+    subtitle: 'Embark on exciting journeys, challenge yourself, and create unforgettable memories',
   },
 ]
 
@@ -44,24 +48,43 @@ const Carousel = ({ autoSlide = true, autoSlideInterval = 3000 }) => {
   }, [autoSlide, autoSlideInterval])
 
   return (
-    <div className="relative w-full max-w-lg mx-auto mt-10 overflow-hidden">
-      <div className="flex transition-transform duration-500 ease-in-out" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+    <div className="relative w-full mx-auto overflow-hidden flex-grow">
+      <div className="flex transition-transform duration-700 ease-in-out" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
         {slides.map((slide, index) => (
-          <div key={index} className="relative min-w-full flex-shrink-0" style={{ height: '500px' }}>
-            <Image src={slide.image} alt={`Slide ${index}`} fill style={{ objectFit: 'cover' }} />
-            <p className="absolute bottom-4 left-4 text-white text-lg bg-black bg-opacity-50 p-2 rounded">{slide.text}</p>
+          <div key={index} className="relative min-w-full flex-shrink-0" style={{ height: 700 }}>
+            <Image src={slide.image} alt={`Slide ${index}`} fill style={{ objectFit: 'cover' }} className="brightness-75" />
+            <div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black  to-transparent text-white text-center">
+              <h2 className="text-2xl font-bold tracking-tight text-white sm:text-3xl ">{slide.title}</h2>
+              <p className="mt-4 text-lg leading-8 text-gray-300 pb-10">{slide.subtitle}</p>
+            </div>
           </div>
         ))}
       </div>
-      <button onClick={prevSlide} className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-white p-2 rounded-full shadow-md">
-        ◀
+      <button onClick={prevSlide} className="absolute left-4 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-35 hover:bg-opacity-100 text-slate-700 p-1 rounded-full shadow-md transition duration-300">
+      <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      height="2em"
+      width="2em"
+    >
+      <path fill="none" d="M0 0h24v24H0z" />
+      <path d="M10.828 12l4.95 4.95-1.414 1.414L8 12l6.364-6.364 1.414 1.414z" />
+    </svg>
       </button>
-      <button onClick={nextSlide} className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-white p-2 rounded-full shadow-md">
-        ▶
+      <button onClick={nextSlide} className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-white bg-opacity-35 hover:bg-opacity-100 text-slate-700 p-1 rounded-full shadow-md transition duration-300">
+      <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      height="2em"
+      width="2em"
+    >
+      <path fill="none" d="M0 0h24v24H0z" />
+      <path d="M13.172 12l-4.95-4.95 1.414-1.414L16 12l-6.364 6.364-1.414-1.414z" />
+    </svg>
       </button>
       <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 flex space-x-2">
         {slides.map((_, index) => (
-          <div key={index} className={`w-2 h-2 rounded-full ${index === currentIndex ? 'bg-white' : 'bg-gray-400'}`}></div>
+          <div key={index} className={`w-3 h-3 rounded-full ${index === currentIndex ? 'bg-white' : 'bg-gray-400'} transition duration-300`}></div>
         ))}
       </div>
     </div>
